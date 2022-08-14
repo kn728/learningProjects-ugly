874cbc949397990e0ac47e0e1890b5c1758335c4
Keegan Naidoo <keegannaidoo3000@gmail.com>
2022-08-14 22:07:37 +1000

    first rendition

---
+++ goodfoods/src/components/navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Typography} from '@mui/material/'
+
+function Navbar() {
+    return (
+        <Typography variant='h1'>hi</Typography>
+      );
+}
+
+export default Navbar;
