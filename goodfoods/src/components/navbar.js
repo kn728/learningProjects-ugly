e0cb5d3abdc274cc1ca4b4fd35ffc84a8953a616
Keegan Naidoo <keegannaidoo3000@gmail.com>
2022-08-16 11:13:11 +1000

    added non functional button

--- goodfoods/src/components/navbar.js
+++ goodfoods/src/components/navbar.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import {AppBar} from '@mui/material/'
+import {AppBar, Button,Toolbar,Typography}  from '@mui/material/'
 import logo from '../images/logo.jpg'
 
 function Navbar() {
     return (
-        <AppBar sx={{backgroundColor:'#d1aa60', width:'100%', height:'10vh', display:'flex', alignContent:'center'}}>
-            <img src={logo} style={{height:'80%', width:'400px', padding:'10px'}}/>
+        <AppBar sx={{backgroundColor:'#d1aa60', width:'100%', height:'10vh'}}>
+            <Toolbar>
+                <Typography  sx= {{
+                      flexGrow: 1,
+                      padding: 2}}>
+                <img src={logo} style={{height:'10vh', width:'400px'}}/>
+                </Typography>
+                <Button  variant="contained" sx={{width:'50px'}}> Hello</Button>
+            </Toolbar>
+            
         </AppBar>
       );
 }
