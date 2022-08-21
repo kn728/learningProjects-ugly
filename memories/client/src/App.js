7d344167fddfc865472bf33df30c368d24fc11b9
Keegan Naidoo <keegannaidoo3000@gmail.com>
2022-08-21 16:57:44 +1000

    add some page structure to app.js

--- memories/client/src/App.js
+++ memories/client/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { Container, AppBar,  Typography, Grow, Grid} from '@mui/material';
+import memories from './images/memories.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth='lg'>
+      <AppBar positiion='static' color='inherit'>
+        <Typography variant='h2' align='center'>Memories</Typography>
+        <img src={memories} />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid justify='space-between' alignItems='stretch' spacing={3}>
+              <Grid item xsw={12} sm={7}>
+                
+              </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 
