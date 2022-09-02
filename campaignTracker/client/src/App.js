2fa78ddd383f85f9ebb18971ac815571d01ed10f
Keegan Naidoo <keegannaidoo3000@gmail.com>
2022-09-02 15:44:16 +1000

    set up campaign app

--- campaignTracker/client/src/App.js
+++ campaignTracker/client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-
+import Navbar from './Components/Navbar';
+import CampaignMap from './Pages/CampaignMap';
 function App() {
   return (
     <div className="App">
-       
+      <Navbar />
+      <CampaignMap />
     </div>
   );
 }
---
+++ campaignTracker/client/src/Components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <div style={{color:'white',backgroundColor:'black', width:'100%', height:'10vh', display:'flex', justifyContent:'space-between'}}>
+            <h1>test</h1>
+            <h2> test</h2>
+        </div>
+    )
+}
