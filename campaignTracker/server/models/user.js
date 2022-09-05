3c35c619b2be5f98f04355655126fff2ad684eca
Keegan Naidoo <keegannaidoo3000@gmail.com>
2022-09-05 20:12:51 +1000

    installed uuid incase i needed to used it

--- campaignTracker/server/models/user.js
+++ campaignTracker/server/models/user.js
@@ -4,10 +4,18 @@ const userSchema = mongoose.Schema({
     name: String,
     userName: {
         type: String,
-        unique: true
+        unique: true,
+        require: [true, 'please enter user name']
     },
-    email: String,
-    password: String
+    email: {
+        type: String,
+        require: [true, 'please enter email address']
+    },
+    password:{
+        type: String,
+        require: [true, 'please enter password']
+    },
+    campaigns:[String]
 })
 
 const userAccounts = mongoose.model('userAccounts', userSchema)
